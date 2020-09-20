fa467585116bcb30cb4b1c9b6eac67c2aacbd828
yutong <yutongzhu9@gmail.com>
2020-09-20 18:57:51 -0400

    add hard authenticate

--- page/src/app/login/login.component.ts
+++ page/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { HardcodedAuthenticationService } from './../service/hardcoded-authentication.service'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,21 +8,23 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  username = "in28"
+  username = "user"
   password = ''
   errorMessage = 'Invalid Credentials'
   invalidLogin = false
 
   //Router
   //Dependency(Router) Injection
-  constructor(private router: Router) { }
+  constructor(private router: Router, 
+    public hardcodedAuthenticationService: HardcodedAuthenticationService) { }
 
   ngOnInit(): void {
   }
 
   handleLogin() {
     // console.log(this.username);
-    if(this.username==="in28" && this.password==='dummy') {
+    // if(this.username==="user" && this.password==='dummy') {
+    if (this.hardcodedAuthenticationService.authenticate(this.username, this.password)) {
       this.router.navigate(['welcome', this.username])
       this.invalidLogin = false
     } else {
